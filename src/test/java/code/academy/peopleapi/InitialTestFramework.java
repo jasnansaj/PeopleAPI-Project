f22f57a4f1eeb56cf57754863f0bd8f0474d8cb8
jasnansaj <jasnansaj@gmail.com>
2021-09-13 02:36:34 -0700

    initial commit

---
+++ src/test/java/code/academy/peopleapi/InitialTestFramework.java
@@ -0,0 +1,15 @@
+package code.academy.peopleapi;
+
+import org.testng.annotations.Test;
+
+public class InitialTestFramework {
+
+    @Test
+
+    public void initialTest() {
+        //make a request to people Api
+        //people Api give back response
+        //checking the body in the response
+
+    }
+}
