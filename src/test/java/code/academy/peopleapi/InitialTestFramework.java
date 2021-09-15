40dd675bbef432e4c502d0e7a20dceccafbdb01a
jasnansaj <jasnansaj@gmail.com>
2021-09-15 02:01:15 -0700

    initial commit

--- src/test/java/code/academy/peopleapi/InitialTestFramework.java
+++ src/test/java/code/academy/peopleapi/InitialTestFramework.java
@@ -1,13 +1,25 @@
 package code.academy.peopleapi;
 
+import client.PeopleApiClient;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.testng.annotations.Test;
 
-public class InitialTestFramework {
+
+public class InitialTestFramework{
+
+    PeopleApiClient peopleApiClient = new PeopleApiClient();
+    HttpResponse response;
+
+
 
     @Test
-
-    public void initialTest() {
+    public void initialTest() throws Exception {
         //make a request to people Api
+       response = peopleApiClient.getWelcomeRequest();
+
+      String body = EntityUtils.toString(response.getEntity());
+
         //people Api give back response
         //checking the body in the response
 
